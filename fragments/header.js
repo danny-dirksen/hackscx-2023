e137126bd594255b9acbddaf65721cb2ff047aa8
Daniel Dirksen <daniel@dirksen.com>
2023-11-04 19:16:15 -0700

    Header update

--- fragments/header.js
+++ fragments/header.js
@@ -2,8 +2,8 @@
 fetch('/fragments/header.html')
   .then(response => response.text())
   .then(data => {
-    const header = document.createElement('header');
-    header.innerHTML = data;
-    document.body.prepend(header);
+    // Add string to beginning of document.body
+    data = data.replaeAll("{title}", document.title)
+    document.body.innerHTML = data + document.body.innerHTML;
   })
   .catch(error => console.log(error));
