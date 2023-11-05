23046f9202a36fb8bc0dd8ce96bbf8e1d9d1ab00
Daniel Dirksen <daniel@dirksen.com>
2023-11-04 21:14:21 -0700

    Added plan explorer

--- fragments/header.js
+++
@@ -1,9 +0,0 @@
-
-fetch('/fragments/header.html')
-  .then(response => response.text())
-  .then(data => {
-    // Add string to beginning of document.body
-    data = data.replaeAll("{title}", document.title)
-    document.body.innerHTML = data + document.body.innerHTML;
-  })
-  .catch(error => console.log(error));
---
+++ js/loadFragments.js
@@ -0,0 +1,12 @@
+const header = document.querySelector('header');
+if (header) {
+  fetch('/fragments/header.html')
+    .then(response => response.text())
+    .then(data => {
+      // Add string to beginning of document.body
+      const headerString = data;
+      header.outerHTML = headerString;
+    })
+    .catch(error => console.error(error));
+}
+
